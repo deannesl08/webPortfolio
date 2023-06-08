c1d37e027751abe5bf55c522b1009912cabf45e7
Deanne Laylay <deannelaylay@gmail.com>
2023-06-08 17:53:21 -0400

    added optimized profile pic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
-app.listen(8080);
+app.listen(9090);
 module.exports = app;
 
-console.log('Server running at http://localhost:8080/');
+console.log('Server running at http://localhost:9090/');
